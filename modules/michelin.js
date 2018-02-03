f7b7af0160ec13c8e931fde081e650e203c5ec8b
Corentin Lemaitre <corentinlemaitre1996@gmail.com>
2018-02-03 13:16:27 +0100

    feat(descriptionReastaurant): Get the description of all starred restaurant michelin guide

--- modules/michelin.js
+++ modules/michelin.js
@@ -1,21 +1,34 @@
 const cheerio = require('cheerio')
 const request = require('request')
 
-const urlLocale = 'file:///C:/Users/cocol/Desktop/Ecoles/Annee4/Semestre%202/Web-Application-Architecture-s-/top-chef/Restaurants%201%20%C3%A9toile%202%20%C3%A9toiles%203%20%C3%A9toiles,%20France%20_%20MICHELIN%20Restaurants.html'
+/* importing the database module */
+const mongodb = require('./mongoDB')
+
+
+
 const urlWeb = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin'
 
 
 exports.getAllRestaurant = (callback) => {
   pagesnumber(urlWeb).then(pages => {
-    // return getRestaurants(1)
     const promises = []
-    for (var i = 1; i <= pages; i++) {
-      promises.push(getRestaurants(i))
+    for (var i = 1; i <= 2; i++) {
+      promises.push(getRestaurantsLinks(i))
     }
     return Promise.all(promises)
-  }).then(links => {
-    //return saveToDatabase(links)
-    callback(null, links)
+  }).then((links) => {
+    const promises = []
+
+    for (var i = 0; i < links.length; i++) {
+      for (var j = 0; j < links[i].length; j++) {
+        promises.push(getDescription(links[i][j]))
+      }
+      console.log(i)
+    }
+    return Promise.all(promises)
+  }).then(restaurants => {
+    // return mongodb.saveRestaurants(restaurants)
+    callback(null, restaurants)
   }).catch(err => {
     callback(err)
   })
@@ -34,13 +47,13 @@ function pagesnumber(url) {
       if ($(pageNumberPath)) {
         resolve(parseInt($(pageNumberPath).next().text()))
       } else {
-        reject(new Error("unable to acces the maximum page number"))
+        reject(new Error("unable to access the maximum page number"))
       }
     })
   })
 }
 
-function getRestaurants(page) {
+function getRestaurantsLinks(page) {
   return new Promise(function(resolve, reject) {
     let urlBis = urlWeb
     if (page > 1) {
@@ -56,11 +69,58 @@ function getRestaurants(page) {
 
       itemsPath = '#panels-content-main-leftwrapper > div.panel-panel.panels-content-main-left > div > div > ul > li'
       let linksOnPage = []
-      $(itemsPath).each(function(i, link){
+      $(itemsPath).each(function(i, link) {
         linksOnPage.push($(link).children().children().attr('href'))
       })
 
       resolve(linksOnPage)
     })
   })
+}
+
+function getDescription(link) {
+  console.log(link)
+  return new Promise(function(resolve, reject) {
+    request.get('https://restaurant.michelin.fr/'+link, (err, res, body) => {
+      if (err) reject("page access error : trying the description of a restaurant \n " + err)
+
+      const $ = cheerio.load(body, {
+        decodeEntities: false
+      })
+
+      item = {}
+
+      pagePath = 'body > div.l-page > div > div.l-main > div > div.panel-display.panels-michelin-content-layout.panels-michelin-2colsidebar.clearfix'
+      if ($(pagePath)) {
+        item.name = clean($(pagePath + '> div.panels-content-main.panels-content-main_regionone > div > div.panels-content-main-left > div > div > div > div > h1').text())
+        item.stars = parseInt($('#node_poi-guide-wrapper > div.node_poi-distinction-section > ul > li:nth-child(1) > div.content-wrapper').text().charAt(0))
+        item.url = link
+        item.address = clean($(pagePath + '> div.panels-content-main.panels-content-main_regionone > div > div.panels-content-main-left > div > div > div > div > div.poi_intro-display-address').text())
+        item.praticalInfo = clean($('#node_poi-info-wrapper > div.node_poi-info.opt-mich__info-items > div').text())
+        item.transport = clean($('#map-location > ul').text())
+        item.phone = clean($('#node_poi-info-wrapper > div.node_poi-contact.node_poi-row.node_poi-general.opt-mich__info-items > div.full-display-contact-details > div.tel').text())
+        item.websiteUrl = clean($('#node_poi-info-wrapper > div.node_poi-contact.node_poi-row.node_poi-general.opt-mich__info-items > div.full-display-contact-details > div.website> a').text())
+        item.cuisineType =clean( $('#node_poi-menu-wrapper > div.node_poi-menu-intro.node_poi-row > div.node_poi-cooking-types').text())
+        item.grade={}
+        item.price = clean($(pagePath + '> div.panels-content-main.panels-content-main_regionone > div > div.panels-content-main-left > div > div > div > div > div.poi_intro-display-prices').text())
+        item.grade.average = parseFloat($('#node_poi-review-wrapper > div.node_poi-review.node_poi-row > div.node_poi-avg-rating > span.avg-rating-points').text())
+        item.grade.tableQuality = parseFloat($('#node_poi-review-wrapper > div.node_poi-review.node_poi-row > div.node_poi-restaurant-rating > div:nth-child(1) > div.poi_node-items-rating > span.avg-rating-points').text())
+        item.grade.service = parseFloat($('#node_poi-review-wrapper > div.node_poi-review.node_poi-row > div.node_poi-restaurant-rating > div:nth-child(2) > div.poi_node-items-rating > span.avg-rating-points').text())
+        item.grade.surroundings = parseFloat($('#node_poi-review-wrapper > div.node_poi-review.node_poi-row > div.node_poi-restaurant-rating > div:nth-child(3) > div.poi_node-items-rating > span.avg-rating-points').text())
+        item.grade.rateQualityPrice = parseFloat($('#node_poi-review-wrapper > div.node_poi-review.node_poi-row > div.node_poi-restaurant-rating > div:nth-child(4) > div.poi_node-items-rating > span.avg-rating-points').text())
+        item.grade.ambiance = parseFloat($('#node_poi-review-wrapper > div.node_poi-review.node_poi-row > div.node_poi-restaurant-rating > div:nth-child(5) > div.poi_node-items-rating > span.avg-rating-points').text())
+        item.grade.drinks = parseFloat($('#node_poi-review-wrapper > div.node_poi-review.node_poi-row > div.node_poi-restaurant-rating > div:nth-child(6) > div.poi_node-items-rating > span.avg-rating-points').text())
+        item.menuLink = clean($(pagePath + '#node_poi-info-wrapper > div.node_poi-contact.node_poi-row.node_poi-general.opt-mich__info-items > div.full-display-contact-details > div.website> a').text())
+
+        console.log(item)
+        resolve(item)
+      } else {
+        reject(new Error("unable to access description of the restaurant" + link))
+      }
+    })
+  })
+}
+
+function clean(string){
+  return string.replace("\n","").replace(/  /g,"")
 }
